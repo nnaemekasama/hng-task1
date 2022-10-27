8d4a7d7f6f654d72bfd530d8f2ef22d4da627668
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-10-27 02:40:08 -1100

    react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HNG task</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>links</div>
+  )
+}
+
+export default Main
