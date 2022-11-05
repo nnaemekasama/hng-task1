4bca596bbac7140e378160ad0dd4275f3be1af88
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-11-04 21:44:10 -1100

    styles

--- src/components/ContactScreen.js
+++ src/components/ContactScreen.js
@@ -23,57 +23,70 @@ const handleChange = (event) => {
 }
 
   return (
-    <form>
-        <>
-        <label>First Name
-            <input
-                id='first_name'
-                type='text'
-                placeholder='Enter your first name'
-                name='firstname'
+
+    <div className='contact-container'>
+        <h2>Contact Me</h2>
+        <p>Hi there contact me to ask me anything you have in mind</p>
+
+        <form className='form-container'>
+            <div className='names'>
+            <label className='name'>
+                <p>First Name</p>
+                <input
+                    id='first_name'
+                    type='text'
+                    placeholder='Enter your first name'
+                    name='firstname'
+                    onChange={handleChange}
+                    value={formData.firstname}
+                />
+            </label>
+            <label className='lastname'>
+                <p>Last Name</p>
+                <input
+                    id='last_name'
+                    type='text'
+                    placeholder='Enter your last name'
+                    name='lastname'
+                    onChange={handleChange}
+                    value={formData.lastname}
+                />
+            </label>
+            </div>
+            <label className='email'>
+                <p>Email</p>
+                <input
+                    id='email'
+                    type='email'
+                    placeholder='yourname@email.com'
+                    name='email'
+                    onChange={handleChange}
+                    value={formData.email}
+                />
+            </label>
+            <label className='message'>
+                <p>Message</p>
+            <textarea
+                id='message'
                 onChange={handleChange}
-                value={formData.firstname}
+                name='comments'
+                value={formData.comments} 
+                placeholder='Send a message and i will reply as soon as possible'
             />
-        </label>
-        <label>Last Name
-            <input
-                id='last_name'
-                type='text'
-                placeholder='Enter your last name'
-                name='lastname'
+            </label>
+            <label className='check'>
+            <input 
+                id='checkbox'
+                type='checkbox'
+                checked={formData.checkbox}
                 onChange={handleChange}
-                value={formData.lastname}
-            />
-        </label>
-        </>
-        <label>Email
-            <input
-                id='email'
-                type='email'
-                placeholder='yourname@email.com'
-                name='email'
-                onChange={handleChange}
-                value={formData.email}
-            />
-        </label>
-        <textarea
-            id='message'
-            onChange={handleChange}
-            name='comments'
-            value={formData.comments} 
-            placeholder='Send a message and i will reply as soon as possible'
-        />
-        <label htmlFor='message'>Message</label>
-        <input 
-            id='checkbox'
-            type='checkbox'
-            checked={formData.checkbox}
-            onChange={handleChange}
-            name='checkbox'
-        />
-        <label htmlFor='checkbox'>You agree to provide your data to nnaemeka_san who may contact you</label>
-        <button id='btn_submit'>Send message</button>
-    </form>
+                name='checkbox'
+            />  
+            <p>You agree to provide your data to nnaemeka_san who may contact you</p>
+            </label>
+            <button className='contact-btn' id='btn_submit'>Send message</button>
+        </form>
+    </div>
   )
 }
 
